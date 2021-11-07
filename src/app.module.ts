4851c96453dee14595c770d6f48596d0027da47b
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-11-08 00:17:35 +0300

    dockerize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,8 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { WatchersModule } from './watchers';
-import { InjectBot } from 'nest-mwn';
-import { mwn } from 'mwn';
 import { SharedModule } from './shared';
 
 @Module({
   imports: [WatchersModule, SharedModule],
 })
-export class AppModule implements OnModuleInit {
-  constructor(
-    @InjectBot()
-    private bot: mwn,
-  ) {}
-
-  async onModuleInit() {
-    await this.bot.login();
-  }
-}
+export class AppModule {}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,5 +1,10 @@
-import { Global, Module } from '@nestjs/common';
-import { MwnModule } from 'nest-mwn';
+import {
+  BeforeApplicationShutdown,
+  Global,
+  Module,
+  OnModuleInit,
+} from '@nestjs/common';
+import { InjectBot, MwnModule } from 'nest-mwn';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MwnConfigService, TypeOrmConfigService } from './services';
 import { LoggingModule } from '@eropple/nestjs-bunyan';
@@ -7,6 +12,7 @@ import { LOGGER } from './logger';
 import * as config from './config';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { mwn } from 'mwn';
 
 @Global()
 @Module({
@@ -28,4 +34,17 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
   ],
   exports: [MwnModule, TypeOrmModule],
 })
-export class SharedModule {}
+export class SharedModule implements OnModuleInit, BeforeApplicationShutdown {
+  constructor(
+    @InjectBot()
+    private bot: mwn,
+  ) {}
+
+  async onModuleInit() {
+    await this.bot.login();
+  }
+
+  async beforeApplicationShutdown() {
+    await this.bot.logout();
+  }
+}
